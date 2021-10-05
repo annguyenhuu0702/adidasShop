3ebd885cd4a646e3a0c6e453ba9c9119a6eb95df
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-05 23:11:36 +0700

    build UI header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./../../assets/css/base.css";
+import "./../../assets/css/header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="header__top">
+              <div className="top__menu">
+                <ul>
+                  <li>
+                    <a href=" ">Trợ giúp</a>
+                  </li>
+                  <li>
+                    <a href=" ">Trình theo dõi đơn hàng</a>
+                  </li>
+                  <li>
+                    <a href=" ">Đăng ký</a>
+                  </li>
+                  <li>
+                    <a href=" ">Đăng nhập</a>
+                  </li>
+                  <li className="image-flag">
+                    <img
+                      src="https://adl-foundation.adidas.com/flags/1-0-0/vn.svg"
+                      alt=""
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="header__bottom">
+              <div className="header__bottom-wrap">
+                <a href=" " className="bottom__logo">
+                  <img
+                    src="https://www.adidas.com.vn/glass/react/fc0a64b/assets/img/icon-adidas-logo.svg"
+                    alt=""
+                  />
+                </a>
+                <div className="bottom__menu">
+                  <div className="menu-navigation">
+                    <a href=" " className="menu-navigation active">
+                      Trang chủ
+                    </a>
+                  </div>
+                  <div className="menu-navigation">
+                    <a href=" ">Nam</a>
+                  </div>
+                  <div className="menu-navigation">
+                    <a href=" ">Nữ</a>
+                  </div>
+                  <div className="menu-navigation">
+                    <a href=" ">Trẻ em</a>
+                  </div>
+                </div>
+                <div className="bottom__right-side-menu">
+                  <div className="search__input-wrapper">
+                    <form>
+                      <input className="form-control" placeholder="Tìm kiếm" />
+                      <span className="search__input-icon">
+                        <i className="far fa-search"></i>
+                      </span>
+                    </form>
+                  </div>
+                  <div className="side-menu__cart">
+                    <i className="far fa-shopping-cart"></i>
+                    <div className="side-menu__cart-qtt">10</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
