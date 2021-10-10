96c729e1a51bb32d42baadc2632c59b20e5f2cab
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2021-10-10 11:11:06 +0700

    update UI page collection

--- src/containers/PageMan/PageMan.js
+++ src/containers/PageMan/PageMan.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./../../assets/css/base.css";
 import "./../../assets/css/pageman.css";
+import { Link } from "react-router-dom";
 
 function PageMan() {
   return (
@@ -18,7 +19,41 @@ function PageMan() {
                 THE TRIPLE BLACK <br /> COLLECTION
               </h2>
               <p>Sneakers to wear with everything</p>
+              <div className="content__button">
+                <Link to="/Triple_black_collection">
+                  <button type="button">
+                    <span>SHOP NOW</span>
+                    <i className="fal fa-long-arrow-right"></i>
+                  </button>
+                </Link>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="page-man__menu">
+        <div className="container">
+          <div className="menu__title d-flex justify-content-center">
+            <h2>NAM</h2>
+          </div>
+          <div className="menu__wrap">
+            <ul>
+              <li>
+                <a href=" ">HOME</a>
+              </li>
+              <li>
+                <a href=" ">GIÀY</a>
+              </li>
+              <li>
+                <a href=" ">QUẦN ÁO</a>
+              </li>
+              <li>
+                <a href=" ">PHỤ KIỆN</a>
+              </li>
+              <li>
+                <a href=" ">TẤT CẢ SẢN PHẨM</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -38,6 +73,14 @@ function PageMan() {
                 Từ mũi giày vỏ sò đến kiểu dáng,​ thiết kế nguyên <br /> bản tạo
                 nên di sản giày sneaker.​
               </p>
+              <div className="content__button">
+                <Link to="/superstart-giay">
+                  <button type="button">
+                    <span>SHOP SUPERSTART</span>
+                    <i className="fal fa-long-arrow-right"></i>
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
